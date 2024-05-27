ecfc1d78878bf26eed4f1c52fa3fc8ae348092f2
Narjis Fatima <161502591+NarjisFatima62@users.noreply.github.com>
2024-05-27 22:05:04 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+"use strict";
+let country = ["Pakistan", "India", "New yourk", "Japan", "China"];
+console.log("list of Country");
+console.log(country);
---
+++ main.ts
@@ -0,0 +1,9 @@
+//Question 20:
+//Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+
+//Answer:
+let country: String[] = ["Pakistan", "India", "New yourk", "Japan", "China"];
+console.log("list of Country");
+console.log(country);
+
